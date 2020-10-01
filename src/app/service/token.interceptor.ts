6cc8a93f79b6360fb3a45149ab6f8d4941462328
dmissud <daniel@missud.net>
2020-10-01 10:34:17 +0200

    intercepteur filtre que le site

--- src/app/service/token.interceptor.ts
+++ src/app/service/token.interceptor.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {AuthService} from './auth.service';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
+  private API = environment.API_CONTACT_URL;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const enrichRequest = request.clone({
-      headers: request.headers.set('Authorization', 'Bearer ' + this.authService.getTokenFromLocalStorage())
-    });
+    let enrichRequest: HttpRequest<unknown> = request;
+    if (request.url.includes(this.API)) {
+      enrichRequest = request.clone({
+        headers: request.headers.set('Authorization', 'Bearer ' + this.authService.getTokenFromLocalStorage())
+      });
+    }
     return next.handle(enrichRequest);
   }
 }
